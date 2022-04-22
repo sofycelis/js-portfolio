57e2bde6c639288835110bded111afeb937a8875
Sofia Celis <sofycelismx@gmail.com>
2022-04-21 22:10:34 -0500

    initial commit

--- src/templates/Template.js
+++ src/templates/Template.js
@@ -1,4 +1,7 @@
-import getData from '../utils/getData.js';
+import getData from '@utils/getData.js';
+import github from "@images/github.png";
+import twitter from "@images/twitter.png";
+import instagram from "@images/instagram.png";
 
 const Template = async () => {
   const data = await getData();
@@ -23,14 +26,14 @@ const Template = async () => {
           </ul>
         </div>
         <div class="card_social">
-          <a href="https://twitter.com/gndx">
-            <img src="../src/assets/images/twitter.png" />
+          <a href="https://twitter.com/SofyCelis">
+            <img src="${twitter}" />
           </a>
           <a href="https://github.com/gndx">
-            <img src="../src/assets/images/github.png" />
+            <img src="${github}" />
           </a>
-          <a href="https://instagram.com/gndx">
-            <img src="../src/assets/images/instagram.png" />
+          <a href="https://www.instagram.com/sofycelis/?hl=es">
+            <img src="${instagram}" />
           </a>
         </div>
       </div>
